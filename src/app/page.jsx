bb56b7f95c6d2e7fc4f83a53ad6018c0d28a4a43
Oscar Castro <oscar.a.castro818@gmail.com>
2024-08-18 00:02:15 -0500

    add ref to component wrapper with id to all main page components/sections, this allows child components to remain RSC and acquire ref to position for nav button scroll to actions

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,22 +1,43 @@
-import Introduction from "@/compnents/Introduction";
-import Testimonials from "@/compnents/Testimonials";
-import Services from "@/compnents/Services";
-import About from "@/compnents/About";
-import FAQ from "@/compnents/FAQ";
-import Contact from "@/compnents/Contact";
+import Introduction from "@/components/Introduction";
+import Testimonials from "@/components/Testimonials";
+import Services from "@/components/Services";
+import About from "@/components/About";
+import FAQ from "@/components/FAQ";
+import Contact from "@/components/Contact";
+import KeepAustinWeird from "@/components/KeepAustinWeird";
+import SmoothScroller from "@/components/SmoothScroller";
+import ErmaBombeck from "@/components/ErmaBombeck";
+import EasyLife from "@/components/EasyLife";
+import RefTo from "@/components/RefTo";
 
 
-export default function Home() {
+const Home = () => {
+
   return (
-      <div>
-          <Introduction/>
+      <>
+          <SmoothScroller/>
+          <RefTo id={"home"}>
+              <Introduction/>
+          </RefTo>
           <Testimonials/>
-          <Services/>
-          <About/>
-          <FAQ/>
-          <Contact/>
-      </div>
+          <RefTo id={"services"}>
+              <Services/>
+          </RefTo>
+          <KeepAustinWeird/>
+          <RefTo id={"about"}>
+              <About/>
+          </RefTo>
+          <ErmaBombeck/>
+          <RefTo id={"faq"}>
+              <FAQ/>
+          </RefTo>
+          <EasyLife/>
+          <RefTo id={"contact"}>
+              <Contact/>
+          </RefTo>
+      </>
 
   );
 }
+export default Home;
 
