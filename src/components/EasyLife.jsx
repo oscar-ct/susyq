cac4fd7932ee6422a7f996a5180cb3f6174a087d
Oscar Castro <oscar.a.castro818@gmail.com>
2024-08-18 23:46:48 -0500

    create new parallax easy life (green) component, used framer for parallax effect

---
+++ src/components/EasyLife.jsx
@@ -0,0 +1,52 @@
+"use client"
+
+import {useRef} from 'react';
+import {motion, useScroll, useTransform} from "framer-motion";
+import {TiStarOutline} from "react-icons/ti";
+import Image from "next/image";
+import Background from "../../public/images/ash-amplifies-NQ6Lh81BTRs-unsplash.jpg";
+import CustomButton from "@/components/CustomButton";
+import {HiLightningBolt} from "react-icons/hi";
+import {useRouter} from "next/navigation";
+
+const EasyLife = () => {
+    const container = useRef();
+    const { scrollYProgress } = useScroll({
+        target: container,
+        offset: ["start end", 'end start']
+    })
+    const y = useTransform(scrollYProgress, [0, 1], ["-10%", "10%"]);
+
+    const router = useRouter();
+    const navigate = () => {
+        router.push("/schedule");
+    };
+
+    return (
+        <div
+            ref={container}
+            className='relative flex items-center justify-center h-[450px] overflow-hidden'
+            style={{clipPath: "polygon(0% 0, 100% 0%, 100% 100%, 0 100%)"}}
+        >
+            <div className='z-10 flex flex-col items-center justify-center text-white'>
+                <TiStarOutline size={80}/>
+                <p className='py-10 w-full text-5xl uppercase text-center md:text-7xl md:w-[900px]'>
+                    Let us make your life a little easier!!!
+                </p>
+                <CustomButton customClass={"h-16"} onClick={navigate}>
+                    <div className={"text-xl flex items-center uppercase truncate"}>
+                        <HiLightningBolt/>
+                        <span className={"pl-3"}>Schedule Now</span>
+                    </div>
+                </CustomButton>
+            </div>
+            <div className='fixed top-[-10vh] left-0 h-[125vh] w-full'>
+                <motion.div style={{y}} className='relative w-full h-full'>
+                    <Image src={Background} fill alt="austin" className={"object-cover"}/>
+                </motion.div>
+            </div>
+        </div>
+    )
+};
+
+export default EasyLife;
