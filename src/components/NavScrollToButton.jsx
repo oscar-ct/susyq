18af78b9cecb990cbcd632a32c5b439b8eeb211f
Oscar Castro <oscar.a.castro818@gmail.com>
2024-08-19 15:17:10 -0500

    update link to handle scroll effect on params update using useeffect

--- src/components/NavScrollToButton.jsx
+++ src/components/NavScrollToButton.jsx
@@ -1,14 +1,30 @@
 "use client"
 
+import Link from "next/link";
+import {useEffect} from "react";
+import {useParams} from "next/navigation";
 
 const NavScrollToButton = ({children, id= ""}) => {
-    const handleScroll = () => {
-        const element = document.getElementById(id);
-        if (element) {
-            element.scrollIntoView({ behavior: 'smooth', block: "center"});
+
+    const params = useParams();
+
+    // const handleScroll = () => {
+    //     const element = document.getElementById(id);
+    //     if (element) {
+    //         element.scrollIntoView({ behavior: 'smooth', block: "center"});
+    //     }
+    // };
+
+    useEffect(() => {
+        if (window.location.hash === `#${id}`) {
+            const element = document.getElementById(id);
+            if (element) {
+                element.scrollIntoView({ behavior: 'smooth', block: "center"});
+            }
         }
-    };
-    return <button  className={"uppercase py-3.5 px-4 flex flex-col items-center hover:bg-cyan-600 hover:text-white transition duration-150 ease-in-out"} onClick={handleScroll}>{children}</button>;
+    }, [params, id])
+
+    return <Link href={`/#${id}`} scroll={false} className={"uppercase py-3.5 px-4 flex flex-col items-center hover:bg-cyan-600 hover:text-white transition duration-150 ease-in-out"}>{children}</Link>;
 };
 
 export default NavScrollToButton;
