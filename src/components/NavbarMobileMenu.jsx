68dcf0c988d82761416ecc6c2028f0e4f6d488ce
Oscar Castro <oscar.a.castro818@gmail.com>
2024-08-19 23:42:26 -0500

    fixed issue causing invisible buttons to accidentally get pressed when nav is closed, added scroll to using use effect hooks and next link components

--- src/components/NavbarMobileMenu.jsx
+++ src/components/NavbarMobileMenu.jsx
@@ -3,15 +3,18 @@ import NavbarMobileMenuItem from "@/components/NavbarMobileMenuItem";
 import {TiHomeOutline, TiMail, TiStarOutline, TiTabsOutline, TiThumbsUp} from "react-icons/ti";
 import CustomButton from "@/components/CustomButton";
 import {useParams, useRouter} from "next/navigation";
+import Link from "next/link";
 import {useEffect, useState} from "react";
 
 const NavbarMobileMenu = ({toggle}) => {
 
     const variants = {
         open: {
+            visibility: "visible",
             transition: { staggerChildren: 0.07, delayChildren: 0.2 }
         },
         closed: {
+            visibility: "hidden",
             transition: { staggerChildren: 0.05, staggerDirection: -1 }
         }
     };
@@ -21,56 +24,60 @@ const NavbarMobileMenu = ({toggle}) => {
     const router = useRouter();
 
     const navigate = () => {
+        toggle();
         router.push("/schedule");
     };
 
-    // const [hash, setHash] = useState("")
-    //
-    //
-    // useEffect(() => {
-    //     if (window.location.hash === `#${hash}`) {
-    //         const element = document.getElementById(hash);
-    //         if (element) {
-    //             element.scrollIntoView({ behavior: 'smooth', block: "center"});
-    //         }
-    //     }
-    // }, [params, hash]);
-    //
+    const [hash, setHash] = useState("");
+
+    useEffect(() => {
+        if (window.location.hash === `#${hash}`) {
+            const element = document.getElementById(hash);
+            if (element) {
+                element.scrollIntoView({ behavior: 'smooth', block: "center"});
+            }
+        }
+    }, [params, hash]);
+
+    const onClick = (id) => {
+        setHash(id);
+        toggle();
+    };
 
 
     return (
         <motion.ul className={"z-50 p-8 absolute top-14 w-72"} variants={variants}>
-            <NavbarMobileMenuItem id={"home"} toggle={toggle}>
-                <div className={"mr-5"}>
+            <NavbarMobileMenuItem>
+                <Link href={"/#home"} scroll={false} onClick={onClick} className={"flex"}>
                     <TiHomeOutline size={iconSize}/>
-                </div>
-                <span className={"text-2xl uppercase"}>Home</span>
+                    <div className={"text-2xl uppercase pl-5 w-full"}>Home</div>
+                </Link>
             </NavbarMobileMenuItem>
-            <NavbarMobileMenuItem id={"services"} toggle={toggle}>
-                <div className={"mr-5"}>
+            <NavbarMobileMenuItem>
+                <Link href={"/#services"} scroll={false} onClick={onClick} className={"flex"}>
                     <TiThumbsUp size={iconSize}/>
-                </div>
-                <span className={"text-2xl uppercase"}>Services</span>
+                    <div className={"text-2xl uppercase pl-5 w-full"}>Services</div>
+                </Link>
             </NavbarMobileMenuItem>
-            <NavbarMobileMenuItem id={"about"} toggle={toggle}>
-                <div className={"mr-5"}>
+            <NavbarMobileMenuItem>
+                <Link href={"/#about"} scroll={false} onClick={onClick} className={"flex"}>
                     <TiStarOutline size={iconSize}/>
-                </div>
-                <span className={"text-2xl uppercase"}>About</span>
+                    <div className={"text-2xl uppercase pl-5 w-full"}>About</div>
+                </Link>
             </NavbarMobileMenuItem>
-            <NavbarMobileMenuItem id={"faq"} toggle={toggle}>
-                <div className={"mr-5"}>
+            <NavbarMobileMenuItem>
+                <Link href={"/#faq"} scroll={false} onClick={onClick} className={"flex"}>
                     <TiTabsOutline size={iconSize}/>
-                </div>
-                <span className={"text-2xl uppercase"}>FAQ</span>
+                    <div className={"text-2xl uppercase pl-5 w-full"}>FAQ</div>
+                </Link>
             </NavbarMobileMenuItem>
-            <NavbarMobileMenuItem id={"contact"} toggle={toggle}>
-                <div className={"mr-5"}>
+            <NavbarMobileMenuItem>
+                <Link href={"/#contact"} scroll={false} onClick={onClick} className={"flex"}>
                     <TiMail size={iconSize}/>
-                </div>
-                <span className={"text-2xl uppercase"}>Contact</span>
+                    <div className={"text-2xl uppercase pl-5 w-full"}>Contact</div>
+                </Link>
             </NavbarMobileMenuItem>
-            <NavbarMobileMenuItem toggle={toggle}>
+            <NavbarMobileMenuItem>
                 <CustomButton customClass={"h-14 bg-white"} onClick={navigate}>
                     <div className={"text-xl flex items-center uppercase truncate"}>
                         <span className={"text-cyan-600"}>Schedule Now</span>
