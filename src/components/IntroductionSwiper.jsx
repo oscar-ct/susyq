75301a3909e856dcbc7c8e9d821906470a19b92c
Oscar Castro <oscar.a.castro818@gmail.com>
2024-08-20 14:19:35 -0500

    adjust icon position to match text using inline class

--- src/components/IntroductionSwiper.jsx
+++ src/components/IntroductionSwiper.jsx
@@ -12,7 +12,7 @@ import { PiHandshake } from "react-icons/pi";
 
 const IntroductionSwiper = () => {
     return (
-        <div className={"z-20 h-full w-[14rem] sm:w-[20rem] md:w-[20rem] lg:w-[30rem] xl:w-[48rem]  xl:pl-20"}>
+        <div className={"z-20 h-full w-[16rem] sm:w-[18rem] md:w-[26rem] lg:w-[34rem] xl:w-[48rem]  xl:pl-20"}>
             <Swiper
                 slidesPerView={1}
                 autoplay={{
@@ -24,20 +24,20 @@ const IntroductionSwiper = () => {
                 fadeEffect={{crossFade: true}}
             >
                 <SwiperSlide>
-                    <div className={"flex flex-col pt-5 md:pt-32"}>
-                        <span className={"text-[6vw] xl:text-[70px] font-light uppercase text-white leading-tight"}>Hello, this is Susy</span>
+                    <div className={"flex flex-col"}>
+                        <span className={"text-[5.5vw] xl:text-[70px] font-light uppercase text-white leading-tight"}>Hello, this is Susy</span>
                         <span className={"text-lg md:text-2xl text-white font-light"}>Call (512) 640-6264</span>
                     </div>
                 </SwiperSlide>
                 <SwiperSlide>
-                    <div className={"flex flex-col pt-5 lg:pt-24"}>
-                        <span className={"text-[6vw] xl:text-[70px] font-light uppercase text-white leading-tight"}>100% Green cleaning <TiLeaf /></span>
+                    <div className={"flex flex-col"}>
+                        <span className={"inline-block text-[5.5vw] xl:text-[70px] font-light uppercase text-white leading-tight"}>100% Green cleaning <TiLeaf className={"inline-block"}/></span>
                         <span className={"text-lg md:text-2xl text-white font-light"}>Beautiful. Clean. Safe.</span>
                     </div>
                 </SwiperSlide>
                 <SwiperSlide>
-                    <div className={"flex flex-col pt-5 lg:pt-32"}>
-                        <span className={"text-[6vw] xl:text-[70px] font-light uppercase text-white leading-tight"}> Satisfaction Guaranteed <PiHandshake /></span>
+                    <div className={"flex flex-col"}>
+                        <span className={"text-[5.5vw] xl:text-[70px] font-light uppercase text-white leading-tight"}> Satisfaction Guaranteed <PiHandshake /></span>
                         {/*<span className={"text-lg md:text-2xl text-white font-light"}>100% Satisfaction</span>*/}
                     </div>
                 </SwiperSlide>
