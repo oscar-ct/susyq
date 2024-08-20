e9397a41aa10a684b7d83a93df1384960c58a11d
Oscar Castro <oscar.a.castro818@gmail.com>
2024-08-20 00:04:19 -0500

    add link to navbar title

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,6 +5,7 @@ import { TiTabsOutline } from "react-icons/ti";
 import { TiMail } from "react-icons/ti";
 import NavbarMobile from "@/components/NavbarMobile";
 import NavScrollToButton from "@/components/NavScrollToButton";
+import Link from "next/link";
 
 
 const Navbar = () => {
@@ -22,10 +23,10 @@ const Navbar = () => {
                 <div className={"h-full flex justify-between max-w-screen-xl mx-auto px-4 lg:px-8 relative"}>
 
 
-                    <div className={"flex items-center"}>
+                    <Link href={"/"} className={"flex items-center"}>
                         <span className={"py-1 text-2xl md:text-4xl text-gray-500 font-light truncate"}>Susy Q Cleaning </span>
                         <TiLeaf size={36} className={"text-lime-400 rotate-12 pl-1"}/>
-                    </div>
+                    </Link>
 
 
                     <div className={"hidden text-gray-500 cursor-pointer sm:flex sm:items-center"}>
