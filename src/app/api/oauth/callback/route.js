a921b0147be649a09b1dc7084e84f694ba0e4889
Oscar Castro <oscar.a.castro818@gmail.com>
2024-09-06 18:40:56 -0500

    fix code issue

--- src/app/api/oauth/callback/route.js
+++ src/app/api/oauth/callback/route.js
@@ -1,28 +1,30 @@
-import axios from "axios";
-import qs from "qs";
+const axios = require('axios');
+const qs = require('qs');
 
 export async function GET (req) {
 
-    const data = qs.stringify({
-        'client_id': 'zI1mK1mtureNyvoHkP',
-        'client_secret': 'XBwNT1qrUz2ls12hSlLq4CnF0c3Q0Brh',
-        'grant_type': 'refresh_token',
-        'code': req.query.code,
-        'user_type': 'Location',
-        'redirect_uri': 'https://susyq.vercel.app/api/oauth/callback'
-    });
-    const config = {
-        method: 'post',
-        maxBodyLength: Infinity,
-        url: 'https://api.servicefusion.com/oauth/access_token',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/x-www-form-urlencoded'
-        },
-        data : data
-    };
+    const code =  req.nextUrl.searchParams.get('code');
+    if (code) {
+        const data = qs.stringify({
+            'client_secret': 'XBwNT1qrUz2ls12hSlLq4CnF0c3Q0Brh',
+            "grant_type": "authorization_code",
+            'client_id': 'zI1mK1mtureNyvoHkP',
+            'code': code,
+            'redirect_uri': 'https://susyq.vercel.app/api/oauth/callback'
+        });
+        const config = {
+            method: 'post',
+            maxBodyLength: Infinity,
+            url: 'https://api.servicefusion.com/oauth/access_token',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            data : data
+        };
 
-    const response = await axios.request(config).catch(err => {});
-
-    return Response.json({ data: response?.data });
+        const response = await axios.request(config).catch(err => {});
+        return Response.json({ data: response?.data });
+    }
+    return new Response("no code")
 }
