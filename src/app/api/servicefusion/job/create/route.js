f97892bd43d11772bf98d2f2061c8fe482800bdc
Oscar Castro <oscar.a.castro818@gmail.com>
2024-09-11 01:35:18 -0500

    test successful, 2nd test

--- src/app/api/servicefusion/job/create/route.js
+++ src/app/api/servicefusion/job/create/route.js
@@ -5,7 +5,9 @@ import Test from "@/models/test";
 export async function POST(req) {
     await connectDB();
     const triggerData = await req.json();
-    await Test.create(triggerData);
-    return new Response("cool", {status: 200})
+    const data = await Test.create({
+        goHighLevelTriggerData: triggerData,
+    });
+    return Response.json(data)
 }
 
