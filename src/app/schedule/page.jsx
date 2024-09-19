ace035dcec68779581737ec761b8636a1b83e82b
Oscar Castro <oscar.a.castro818@gmail.com>
2024-09-19 14:00:44 -0500

    add parent global state provider, add schedule form component, add guarantees component, styles and title

--- src/app/schedule/page.jsx
+++ src/app/schedule/page.jsx
@@ -1,9 +1,24 @@
+import ScheduleGuarantees from "@/components/ScheduleGuarantees";
+import ScheduleForm from "@/components/ScheduleForm";
+import {GlobalProvider} from "@/context/GlobalContext";
 
 const SchedulePage = () => {
     return (
-        <div>
-            Schedule Now
-        </div>
+        <GlobalProvider>
+            <section className={"w-full mx-auto px-4 lg:px-8 max-w-screen-xl"}>
+                <div className={"text-center text-5xl uppercase font-thin text-gray-500 py-20"}>
+                    Customize Your Cleaning
+                </div>
+                <div className={"flex flex-col lg:flex-row"}>
+                    <div className={"w-full lg:w-9/12"}>
+                        <ScheduleForm/>
+                    </div>
+                    <div className={"w-full h-full lg:w-3/12"}>
+                        <ScheduleGuarantees/>
+                    </div>
+                </div>
+            </section>
+        </GlobalProvider>
     );
 };
 
