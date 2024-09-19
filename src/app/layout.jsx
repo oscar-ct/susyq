b7cd720f0c6afd07b17369e68bb61caac6fe58e0
Oscar Castro <oscar.a.castro818@gmail.com>
2024-09-19 13:58:26 -0500

    add min height to layout to fill viewport height - footer height

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -15,7 +15,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
         <body className={inter.className}>
             <Navbar/>
-            <main className={"mt-[58px] sm:mt-[75px]"}>{children}</main>
+            <main className={"mt-[58px] sm:mt-[75px] min-h-[calc(100vh-326px)] sm:min-h-[calc(100vh-203px)]"}>{children}</main>
             <Footer/>
         </body>
     </html>
