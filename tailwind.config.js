82d9cf1cc1064379afedcea4714efc3282727edb
Oscar Castro <oscar.a.castro818@gmail.com>
2024-09-21 15:04:43 -0500

    create custom tailwindcss class for custom color

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        'susy': "rgb(79, 175, 194)"
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
