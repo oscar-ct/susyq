f96e1fc3c4cfc02de342da13eba071abd1975414
Oscar Castro <oscar.a.castro818@gmail.com>
2024-09-21 03:43:14 -0500

    minor style update, create new handler for frequency state, add new frequency select

--- src/components/ScheduleFormPanelDetails.jsx
+++ src/components/ScheduleFormPanelDetails.jsx
@@ -7,7 +7,7 @@ import ArrowSvg from "@/components/ArrowSvg";
 
 const ScheduleFormPanelDetails = () => {
 
-    const { serviceDetails, dispatch } = useContext(GlobalContext);
+    const { services, serviceDetails, dispatch } = useContext(GlobalContext);
     const extraOptions = [
         ["Refrigerator", "Refrigerator Cleaning Service"], ["Oven", "Kitchen Oven Cleaning Service"], ["Window", "Window Cleaning Service"]
     ];
@@ -62,18 +62,26 @@ const ScheduleFormPanelDetails = () => {
         dispatchServiceDetails(details);
     };
 
+    const setFrequencyChange = (e) => {
+        const details = {...serviceDetails, frequency: e.target.value}
+        dispatchServiceDetails(details);
+    };
+
     return (
         <div className={"px-2 py-8 md:p-8"}>
-            <div className={"flex flex-col pb-8 md:pb-2"}>
-                <div className={"border-b pb-2 text-2xl text-center md:text-start"}>House</div>
-                <div className="w-full py-3 flex flex-col gap-4">
-                    <div className={"flex flex-col items-center gap-4 md:flex-row"}>
-                        <div className="tracking-wide font-semibold text-gray-700 md:w-20">
-                            Rooms
-                        </div>
+            <div className={"md:pt-3 pb-4 md:pb-2"}>
+                {/*<div className={border-b pb-2 text-2xl text-center md:text-start"}>House Size</div>*/}
+                <div className={"pb-6 flex flex-col items-center gap-4 md:flex-row"}>
+                    <div className="hidden md:block tracking-wide font-semibold text-gray-700 md:w-2/12">
+                        Rooms
+                    </div>
+                    <div className="md:hidden text-xl tracking-wide font-semibold text-gray-700">
+                        House Size
+                    </div>
+                    <div className={"w-full flex flex-col items-center gap-4 md:flex-row md:w-10/12"}>
                         <div className="relative w-full md:w-6/12">
                             <select
-                                className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none hover:bg-gray-300"
+                                className="cursor-pointer block appearance-none w-full bg-stone-100 border border-stone-200 text-gray-700 py-3 px-4 rounded leading-tight focus:outline-none hover:bg-stone-200"
                                 id="bedroom"
                                 onChange={(e) => setBedrooms(e)}
                                 defaultValue={serviceDetails.rooms.bedroom}
@@ -89,7 +97,7 @@ const ScheduleFormPanelDetails = () => {
                         </div>
                         <div className="relative w-full md:w-6/12">
                             <select
-                                className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none hover:bg-gray-300"
+                                className="cursor-pointer block appearance-none w-full bg-stone-100 border border-stone-200 text-gray-700 py-3 px-4 rounded leading-tight focus:outline-none hover:bg-stone-200"
                                 id="bathroom"
                                 onChange={(e) => setBathrooms(e)}
                                 defaultValue={serviceDetails.rooms.bathroom}
@@ -103,68 +111,93 @@ const ScheduleFormPanelDetails = () => {
                             <ArrowSvg/>
                         </div>
                     </div>
-                    <div className={"flex flex-col items-center gap-4 md:flex-row"}>
-                        <div className="py-3 tracking-wide font-semibold text-gray-700 md:w-20">
-                            Size
-                        </div>
-                        <div className={"py-3 flex w-full md:w-6/12"}>
-                            <input
-                                onChange={handleCheckboxChange}
-                                checked={checkboxSelected}
-                                type={"checkbox"}
-                                className={"cursor-pointer"}
-                            />
-                            <div className={"pl-4 cursor-pointer tracking-wide font-semibold text-gray-700"} onClick={handleCheckboxChange}>
-                                I know my square footage
-                            </div>
-                        </div>
+                </div>
 
-                        <div className="relative w-full md:w-6/12">
-                            {
-                                checkboxSelected && (
-                                    <>
-                                        <select
-                                            className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none hover:bg-gray-300"
-                                            id="sq"
-                                            onChange={(e) => handleSqFtChange(e)}
-                                            defaultValue={serviceDetails.size}
-                                        >
-                                            <option value={"< 500"}>&#60; 500 sq ft</option>
-                                            <option value={"500 - 1000"}>500 - 1000 sq ft</option>
-                                            <option value={"1000 - 2000"}>1000 - 2000 sq ft</option>
-                                            <option value={"2000 - 3000"}>2000 - 3000 sq ft</option>
-                                            <option value={"3000 - 4000"}>3000 - 4000 sq ft</option>
-                                            <option value={"4000 - 5000"}>4000 - 5000 sq ft</option>
-                                            <option value={"5000 +"}>5000 + sq ft</option>
-                                        </select>
-                                        <ArrowSvg/>
-                                    </>
-                                )
-                            }
+                <div className={"md:pb-6 flex flex-col items-center gap-4 md:flex-row"}>
+                    <div className="hidden pr-4 md:block py-3 tracking-wide font-semibold text-gray-700 md:w-2/12">
+                        House Size
+                    </div>
+                    <div className={"py-3 flex w-full md:w-5/12"}>
+                        <input
+                            onChange={handleCheckboxChange}
+                            checked={checkboxSelected}
+                            type={"checkbox"}
+                            className={"cursor-pointer"}
+                        />
+                        <div className={"pl-4 cursor-pointer tracking-wide font-semibold text-gray-700"}
+                             onClick={handleCheckboxChange}>
+                            I know my square footage
                         </div>
                     </div>
+
+                    <div className="relative w-full md:w-5/12">
+                        {
+                            checkboxSelected && (
+                                <>
+                                    <select
+                                        className="cursor-pointer block appearance-none w-full bg-stone-100 border border-stone-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none hover:bg-stone-200"
+                                        id="sq"
+                                        onChange={(e) => handleSqFtChange(e)}
+                                        defaultValue={serviceDetails.size}
+                                    >
+                                        <option value={"< 500"}>&#60; 500 sq ft</option>
+                                        <option value={"500 - 1000"}>500 - 1000 sq ft</option>
+                                        <option value={"1000 - 2000"}>1000 - 2000 sq ft</option>
+                                        <option value={"2000 - 3000"}>2000 - 3000 sq ft</option>
+                                        <option value={"3000 - 4000"}>3000 - 4000 sq ft</option>
+                                        <option value={"4000 - 5000"}>4000 - 5000 sq ft</option>
+                                        <option value={"5000 +"}>5000 + sq ft</option>
+                                    </select>
+                                    <ArrowSvg/>
+                                </>
+                            )
+                        }
+                    </div>
                 </div>
+                {
+                    services.includes("Weekly, Bi-Weekly, Monthly") && (
+                        <div className={"pb-6 flex flex-col items-center gap-4 md:flex-row"}>
+                            <div className="text-xl md:text-base tracking-wide font-semibold text-gray-700 md:w-2/12">
+                                Frequency
+                            </div>
+                            <div className="relative w-full md:w-10/12">
+                                <select
+                                    className="cursor-pointer block appearance-none w-full bg-stone-100 border border-stone-200 text-gray-700 py-3 px-4 rounded leading-tight focus:outline-none hover:bg-stone-200"
+                                    id="frequency"
+                                    onChange={(e) => setFrequencyChange(e)}
+                                    defaultValue={serviceDetails.frequency}
+                                >
+                                    <option value={"Weekly"}>Weekly</option>
+                                    <option value={"Bi-Weekly"}>Bi-Weekly</option>
+                                    <option value={"Monthly"}>Monthly</option>
+                                </select>
+                                <ArrowSvg/>
+                            </div>
+                        </div>
+                    )
+                }
             </div>
 
             <div className={"flex flex-col"}>
-                <div className={"border-b py-2 text-2xl text-center md:text-start"}>Add Extras</div>
-                    <div className={"w-full px-3 md:px-0"}>
-                        <div className={"py-4 flex flex-col md:flex-row gap-4"}>
-                            {
-                                extraOptions.map(([title, description]) => {
-                                    return (
-                                        <div onClick={() => handleExtraChange(description)} key={title}
-                                             className={`${serviceDetails.extras.includes(description) ? "bg-cyan-600 text-white" : "hover:bg-zinc-100"} cursor-pointer flex flex-col items-center gap-4 p-4 text-center border-2 h-48 w-full lg:w-4/12`}>
-                                            {
-                                                title === "Refrigerator" ?
-                                                    <CgSmartHomeRefrigerator size={100}/> : title === "Oven" ?
-                                                        <PiOven size={100}/> : <GiWindow size={100}/>
-                                            }
-                                            <span className={`${serviceDetails.extras.includes(description) ? "text-white" : "text-cyan-600"}`}>{description}</span>
-                                        </div>
-                                    )
-                                })
-                            }
+                <div className={"border-b py-2 text-xl text-center font-semibold"}>Need an extra service?</div>
+                <div className={"w-full px-3 md:px-0"}>
+                    <div className={"pt-8 flex flex-col md:flex-row gap-4"}>
+                        {
+                            extraOptions.map(([title, description]) => {
+                                return (
+                                    <div onClick={() => handleExtraChange(description)} key={title}
+                                         className={`${serviceDetails.extras.includes(description) ? "!bg-cyan-600 text-white" : "hover:bg-stone-200"} cursor-pointer flex flex-col items-center gap-4 p-4 rounded text-center bg-stone-100 border border-stone-200 h-44 w-full lg:w-4/12`}>
+                                        {
+                                            title === "Refrigerator" ?
+                                                <CgSmartHomeRefrigerator size={80}/> : title === "Oven" ?
+                                                    <PiOven size={80}/> : <GiWindow size={80}/>
+                                        }
+                                        <span
+                                            className={`${serviceDetails.extras.includes(description) ? "text-white" : "text-cyan-600"}`}>{description}</span>
+                                    </div>
+                                )
+                            })
+                        }
                     </div>
                 </div>
             </div>
