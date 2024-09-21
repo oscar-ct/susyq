2945b7af1f6bd9101ed262b4f67d4ed4d8fa40eb
Oscar Castro <oscar.a.castro818@gmail.com>
2024-09-21 18:18:00 -0500

    update flex position

--- src/components/ServicesItem.jsx
+++ src/components/ServicesItem.jsx
@@ -2,7 +2,7 @@ import ServiceIcon from "@/components/ServiceIcon";
 
 const ServicesItem = ({ title, content, iconName, iconSize = 75 } ) => {
     return (
-        <div className={`py-5 md:py-6 w-full md:w-6/12 flex`}>
+        <div className={`py-5 md:py-6 w-full md:w-6/12 flex items-center`}>
             <div className={"px-8 flex justify-center items-start text-susy"}>
                 <ServiceIcon iconName={iconName} iconSize={iconSize}/>
             </div>
