c7b5821f23eac3b0394635e1fd9105ea5af7636b
Oscar Castro <oscar.a.castro818@gmail.com>
2024-09-23 22:20:34 -0500

    minor title style update

--- src/app/schedule/page.jsx
+++ src/app/schedule/page.jsx
@@ -6,7 +6,7 @@ const SchedulePage = () => {
     return (
         <GlobalProvider>
             <section className={"w-full mx-auto px-4 lg:px-8 max-w-screen-xl"}>
-                <div className={"py-10 text-center text-5xl uppercase font-thin text-gray-500 sm:py-20"}>
+                <div className={"py-10 text-center text-5xl uppercase font-light text-gray-500 sm:py-20"}>
                     Customize Your Cleaning
                 </div>
                 <div className={"flex flex-col lg:flex-row"}>
