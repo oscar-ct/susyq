288ba2434f15d04f020305b83eee4ef8cd5feaf2
Oscar Castro <oscar.a.castro818@gmail.com>
2024-09-25 16:57:50 -0500

    use lenis react with children prop

--- src/components/SmoothScroller.jsx
+++ src/components/SmoothScroller.jsx
@@ -1,20 +1,14 @@
 "use client"
 
-import {useEffect} from "react";
-import Lenis from "lenis";
+import { ReactLenis } from 'lenis/react'
 
-const SmoothScroller = () => {
+const SmoothScroller = ({children}) => {
 
-    useEffect( () => {
-        const lenis = new Lenis()
-        function raf(time) {
-            lenis.raf(time)
-            requestAnimationFrame(raf)
-        }
-        requestAnimationFrame(raf)
-    });
-
-    return null;
+    return (
+        <ReactLenis root>
+            {children}
+        </ReactLenis>
+    )
 };
 
 export default SmoothScroller;
