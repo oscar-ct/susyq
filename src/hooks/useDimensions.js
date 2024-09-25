073dbfd1d7b581a08d95d16ec7f80e8e13f7d54a
Oscar Castro <oscar.a.castro818@gmail.com>
2024-09-25 13:20:12 -0500

    add missing dep to use effect

--- src/hooks/useDimensions.js
+++ src/hooks/useDimensions.js
@@ -6,7 +6,7 @@ const UseDimensions = (ref) => {
     useEffect(() => {
         dimensions.current.width = ref.current.offsetWidth;
         dimensions.current.height = ref.current.offsetHeight;
-    }, []);
+    }, [ref]);
 
     return dimensions.current;
 };
