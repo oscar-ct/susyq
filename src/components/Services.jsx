ab0f2e07948b3ed965d3493f77bcea754c30d027
Oscar Castro <oscar.a.castro818@gmail.com>
2024-10-30 17:12:49 -0500

    update services description and update element syntax

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -9,15 +9,15 @@ const Services = () => {
         ["TiBriefcase", "Commercial Cleaning", "Susy Q understands the importance of your company’s image and your office’s appearance."],
         ["TiArrowMinimiseOutline", "Move in Cleaning", "We understand the stress of moving your home lets us take care of the cleaning!."],
         ["TiArrowMaximiseOutline", "Move Out Cleaning", "Don’t want to clean up or be charged a fortune after moving out, our team works hard and we put every effort to meet your expectations."],
-        ["TiShoppingBag", "Organization", "Organization is key to keeping a tidy home, let us help."],
+        ["TiShoppingBag", "Organization", "Organization is key to keeping a tidy home, let us help unclutter your home."],
     ]
 
     return (
         <section className={"py-14 w-full mx-auto px-4 lg:px-8 max-w-screen-xl"}>
             <div className={"flex flex-col items-center"}>
                 <div className={"flex flex-col justify-center pb-8 md:pb-4"}>
-                    <span className={"text-center text-5xl uppercase font-light text-gray-500"}>Our Services</span>
-                    <span className={"pt-5 text-center text-xl font-light text-susy"}>We are doing a couple of things you might be interested in</span>
+                    <h1 className={"text-center text-5xl uppercase font-light text-gray-500"}>Our Services</h1>
+                    <h4 className={"pt-5 text-center text-2xl font-light text-susy"}>We are doing a couple of things you might be interested in</h4>
                 </div>
                 <div className={"flex flex-col md:flex-row flex-wrap"}>
                     {
