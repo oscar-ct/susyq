1fe24c192aba297fad2021c3abd06ce740f08bd5
Oscar Castro <oscar.a.castro818@gmail.com>
2024-10-30 04:30:21 -0500

    add border to nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,7 +19,7 @@ const Navbar = () => {
             </div>
 
 
-            <div className={"fixed h-[58px] sm:h-[76px] bg-white inset-0 z-30"}>
+            <div className={"fixed h-[58px] sm:h-[76px] bg-white inset-0 z-30 border-b"}>
                 <div className={"h-full flex justify-between max-w-screen-xl mx-auto px-4 lg:px-8 relative"}>
 
 
