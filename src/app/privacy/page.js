cd490e484b6e0491d3265107e9f0ccbfe584b528
Oscar Castro <oscar.a.castro818@gmail.com>
2024-10-30 17:19:34 -0500

    create new routes and pages for privacy policy and terms and conditions

---
+++ src/app/privacy/page.js
@@ -0,0 +1,10 @@
+
+const Page = () => {
+    return (
+        <div className={"text-7xl"}>
+            Privacy Policy Page
+        </div>
+    );
+};
+
+export default Page;
---
+++ src/app/terms/page.js
@@ -0,0 +1,10 @@
+
+const Page = () => {
+    return (
+        <div className={"text-7xl"}>
+            Terms and Conditions Page
+        </div>
+    );
+};
+
+export default Page;
