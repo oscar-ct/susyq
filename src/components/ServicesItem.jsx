02459e30714b9badd67e006be5e77a1eaf59a1ff
Oscar Castro <oscar.a.castro818@gmail.com>
2024-10-30 17:13:13 -0500

    remove excess whitespace

--- src/components/ServicesItem.jsx
+++ src/components/ServicesItem.jsx
@@ -3,7 +3,7 @@ import ServiceIcon from "@/components/ServiceIcon";
 const ServicesItem = ({ title, content, iconName, iconSize = 75 } ) => {
     return (
         <div className={`py-5 md:py-6 w-full md:w-6/12`}>
-            <div className={"flex flex-col items-center text-gray-500 font-light  gap-3"}>
+            <div className={"flex flex-col items-center text-gray-500 font-light gap-3"}>
                 <span className={"lg:hidden text-2xl"}>{title}</span>
                 <div className={"flex items-center"}>
                     <div className={"px-8 flex justify-center items-start text-susy"}>
@@ -15,7 +15,6 @@ const ServicesItem = ({ title, content, iconName, iconSize = 75 } ) => {
                             {content}
                         </p>
                     </div>
-
                 </div>
             </div>
         </div>
