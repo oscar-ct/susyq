d45a485cc2ed794767b326c2501425d942829a47
Oscar Castro <oscar.a.castro818@gmail.com>
2024-10-30 04:08:21 -0500

    update element syntax and minor style update

--- src/components/IntroductionSwiper.jsx
+++ src/components/IntroductionSwiper.jsx
@@ -12,7 +12,7 @@ import { PiHandshake } from "react-icons/pi";
 
 const IntroductionSwiper = () => {
     return (
-        <div className={"z-20 h-full w-[16rem] sm:w-[18rem] md:w-[26rem] lg:w-[34rem] xl:w-[48rem]  xl:pl-20"}>
+        <div className={"h-full w-full"}>
             <Swiper
                 slidesPerView={1}
                 autoplay={{
@@ -24,21 +24,22 @@ const IntroductionSwiper = () => {
                 fadeEffect={{crossFade: true}}
             >
                 <SwiperSlide>
-                    <div className={"flex flex-col"}>
-                        <span className={"text-2xl sm:text-[5.5vw] xl:text-[70px] font-light uppercase text-white leading-tight"}>Hello, this is Susy</span>
-                        <span className={"text-lg md:text-2xl text-white font-light"}>Call (512) 640-6264</span>
+                    <div className={"flex flex-col md:text-center text-white"}>
+                        <h1 className={"text-2xl sm:text-[5.5vw] xl:text-[70px] font-light uppercase leading-tight"}>Hello,
+                            this is Susy</h1>
+                        <h4 className={"text-lg md:text-2xl font-light"}>Call <a href={"tel:512-640-6264"} className={"underline"}>512-640-6264</a></h4>
                     </div>
                 </SwiperSlide>
                 <SwiperSlide>
-                    <div className={"flex flex-col"}>
-                        <span className={"inline-block text-2xl sm:text-[5.5vw] xl:text-[70px] font-light uppercase text-white leading-tight"}>100% Green cleaning <TiLeaf className={"inline-block"}/></span>
-                        <span className={"text-lg md:text-2xl text-white font-light"}>Beautiful. Clean. Safe.</span>
+                    <div className={"flex flex-col md:text-center text-white"}>
+                        <h1 className={"inline-block text-2xl sm:text-[5.5vw] xl:text-[70px] font-light uppercase leading-tight"}>100% Green cleaning <TiLeaf className={"text-lime-400 inline-block"}/></h1>
+                        <h4 className={"text-lg md:text-2xl font-light"}>Beautiful. Clean. Safe.</h4>
                     </div>
                 </SwiperSlide>
                 <SwiperSlide>
-                    <div className={"flex flex-col"}>
-                        <span className={"text-2xl sm:text-[5.5vw] xl:text-[70px] font-light uppercase text-white leading-tight"}> Satisfaction Guaranteed <PiHandshake /></span>
-                        {/*<span className={"text-lg md:text-2xl text-white font-light"}>100% Satisfaction</span>*/}
+                    <div className={"flex flex-col md:text-center text-2xl sm:text-[5.5vw] xl:text-[70px] font-light uppercase text-white leading-tight"}>
+                        <h1>Satisfaction Guaranteed</h1>
+                        <div className={"flex md:justify-center"}> <PiHandshake/></div>
                     </div>
                 </SwiperSlide>
             </Swiper>
