34b7f182344ec29a3f705fc51fa101369a01ff76
Oscar Castro <oscar.a.castro818@gmail.com>
2024-10-30 17:15:55 -0500

    update intro swiper, add use effect to handle window width state, add pagination styles, remove text centering, and update text size

--- src/components/IntroductionSwiper.jsx
+++ src/components/IntroductionSwiper.jsx
@@ -1,16 +1,30 @@
 "use client"
 
 import { Swiper, SwiperSlide} from "swiper/react";
-import {Autoplay, EffectFade} from "swiper/modules";
+import {Autoplay, EffectFade, Pagination} from "swiper/modules";
 import 'swiper/css';
 import 'swiper/css/autoplay';
 import 'swiper/css/effect-fade';
+import 'swiper/css/pagination';
 import { TiLeaf } from "react-icons/ti";
-import { PiHandshake } from "react-icons/pi";
+import {useEffect, useState} from "react";
 
 
 
 const IntroductionSwiper = () => {
+
+    const [windowWidth, setWindowWidth] = useState(0);
+
+    useEffect(() => {
+        if (windowWidth === 0) setWindowWidth(window.innerWidth);
+        function handleResize() {
+            setWindowWidth(window.innerWidth);
+        }
+        window.addEventListener("resize", handleResize);
+        return () => window.removeEventListener("resize", handleResize);
+    }, [windowWidth]);
+
+
     return (
         <div className={"h-full w-full"}>
             <Swiper
@@ -19,27 +33,28 @@ const IntroductionSwiper = () => {
                     delay: 10500,
                     disableOnInteraction: false
                 }}
-                modules={[EffectFade, Autoplay]}
+                modules={[EffectFade, Autoplay, Pagination]}
                 effect={"fade"}
                 fadeEffect={{crossFade: true}}
+                pagination={windowWidth > 1024}
             >
                 <SwiperSlide>
-                    <div className={"flex flex-col md:text-center text-white"}>
-                        <h1 className={"text-2xl sm:text-[5.5vw] xl:text-[70px] font-light uppercase leading-tight"}>Hello,
+                    <div className={"flex flex-col text-white"}>
+                        <h1 className={"text-2xl sm:text-[6vw] xl:text-8xl font-light uppercase leading-tight"}>Hello,
                             this is Susy</h1>
                         <h4 className={"text-lg md:text-2xl font-light"}>Call <a href={"tel:512-640-6264"} className={"underline"}>512-640-6264</a></h4>
                     </div>
                 </SwiperSlide>
                 <SwiperSlide>
-                    <div className={"flex flex-col md:text-center text-white"}>
-                        <h1 className={"inline-block text-2xl sm:text-[5.5vw] xl:text-[70px] font-light uppercase leading-tight"}>100% Green cleaning <TiLeaf className={"text-lime-400 inline-block"}/></h1>
+                    <div className={"flex flex-col text-white"}>
+                        <h1 className={"inline-block text-2xl sm:text-[6vw] xl:text-8xl font-light uppercase leading-tight"}>100% Green cleaning <TiLeaf className={"text-lime-400 inline-block"}/></h1>
                         <h4 className={"text-lg md:text-2xl font-light"}>Beautiful. Clean. Safe.</h4>
                     </div>
                 </SwiperSlide>
                 <SwiperSlide>
-                    <div className={"flex flex-col md:text-center text-2xl sm:text-[5.5vw] xl:text-[70px] font-light uppercase text-white leading-tight"}>
+                    <div className={"flex flex-col text-2xl sm:text-[6vw] xl:text-8xl font-light uppercase text-white leading-tight"}>
                         <h1>Satisfaction Guaranteed</h1>
-                        <div className={"flex md:justify-center"}> <PiHandshake/></div>
+                        {/*<div className={"text-7xl"}><PiHandshake/></div>*/}
                     </div>
                 </SwiperSlide>
             </Swiper>
