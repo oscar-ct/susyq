c83ffa461ef84b8aedcf77822e831810a008add2
Oscar Castro <oscar.a.castro818@gmail.com>
2024-10-30 23:12:58 -0500

    update loading page

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -1,11 +1,7 @@
-export const metadata = {
-    title: "e-shop | Loading...",
-};
-
 const Loading = () => {
     return (
-        <div className={"z-30 w-full mx-auto min-h-[calc(100vh-292px)] md:min-h-[calc(100vh-224px)] flex items-center justify-center"}>
-            <span className="loading loading-bars loading-lg"/>
+        <div className={"z-30 bg- w-full mx-auto min-h-[calc(100vh-388px)] md:min-h-[calc(100vh-251px)] flex items-center justify-center"}>
+            <span className="loading-lg"/>
         </div>
     );
 };
