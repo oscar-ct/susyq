c9ffa7ff065fccfc294c0cfb52241b96289507c0
Oscar Castro <oscar.a.castro818@gmail.com>
2024-10-31 15:20:20 -0500

    install vercel analytics and add react component to layout

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,6 +14,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+        <Analytics/>
         <body className={inter.className}>
             <Navbar/>
             <main className={"mt-[58px] md:mt-[75px] min-h-[calc(100vh-388px)] md:min-h-[calc(100vh-251px)]"}>{children}</main>
