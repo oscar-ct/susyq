2661e3fcd75f402b93f520e418720b227883b157
Oscar Castro <oscar.a.castro818@gmail.com>
2024-10-30 23:11:43 -0500

    setup privacy page title and meta data

---
+++ src/app/privacy/page.jsx
@@ -0,0 +1,22 @@
+export const metadata = {
+    title: "Privacy Policy | Susy Q Cleaning",
+    description: "Susy Q cleaning is a professional company dedicated to be part of your life. Our interest are focused on meeting your needs by offering high quality services and high level satisfaction to our customers. Our green healthy cleaning techniques provide your home with a cleaner healthier environment.",
+};
+
+const Page = () => {
+    return (
+        <section className={"py-14 w-full mx-auto px-4 lg:px-8 max-w-screen-xl"}>
+            <div className={"flex flex-col items-center"}>
+                <div className={"flex flex-col justify-center pb-8 md:pb-4"}>
+                    <h1 className={"text-center text-5xl uppercase font-light text-gray-500"}>Privacy Policy</h1>
+                    <h4 className={"pt-5 text-center text-2xl font-light text-gray-500"}>Last updated: October 1st, 2024</h4>
+                </div>
+                <div className={"flex flex-col md:flex-row"}>
+                    <p>Some content here...</p>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Page;
