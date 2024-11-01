4675c60f02b29ddd35f36cbc165a9420233b177f
Oscar Castro <oscar.a.castro818@gmail.com>
2024-11-01 15:45:45 -0500

    update variable to var from let to create property on the global object, add try catch to set promise to null, write basic backup code to fix any future possible connection issues

--- src/lib/connectDB.js
+++ src/lib/connectDB.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-let cached = global.mongoose;
+var cached = global.mongoose;
 if (!cached) {
     cached = global.mongoose = { conn: null, promise: null };
 }
@@ -17,8 +17,33 @@ async function connectDB() {
             return mongoose;
         });
     }
-    cached.conn = await cached.promise;
+    try {
+        cached.conn = await cached.promise
+    } catch (e) {
+        cached.promise = null
+        throw e
+    }
     return cached.conn;
 }
+
+
+// let connected = false;
+// const connectDB = async () => {
+//     // strictQuery allows only fields specified in out schema to be saved to our database
+//     mongoose.set("strictQuery", true);
+//     // if database is already connected, don't connect again
+//     if (connected) {
+//         console.log("MongoDB is connected!");
+//         return;
+//     }
+//     // connect to mongodb
+//     try {
+//         await mongoose.connect(process.env.MONGO_URI);
+//         connected = true;
+//     } catch (e) {
+//         console.log(e)
+//     }
+// }
+
 
 export default connectDB;
