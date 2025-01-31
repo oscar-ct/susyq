3dc2c5630eb122231c5117cbb98853396dd75f6c
Oscar Castro <oscar.a.castro818@gmail.com>
2025-01-30 19:13:04 -0600

    remove validation, update var names, refactor dup code, add new buttons for new frequency tab

--- src/components/ServiceFormButtons.jsx
+++ src/components/ServiceFormButtons.jsx
@@ -4,39 +4,42 @@ import useNavTo from "@/hooks/useNavTo";
 
 const ServiceFormButtons = () => {
 
-    const { services, activeTab, serviceDetails, serviceContact, serviceNotes, dispatch, serviceSubmitted, tabs, submitInProgress, serviceSource } = useContext(GlobalContext);
-    const { navToServiceDetails, navToServiceContact, navToPrev, navToServiceNotes } = useNavTo();
+    const { frequency, services, activeTab, serviceDetails, serviceContact, serviceNotes, dispatch, hasSubmittedEstimateSuccessfully, tabs, isAttemptingToSubmitEstimate, serviceSource } = useContext(GlobalContext);
+    const { navToServices, navToServiceDetails, navToServiceContact, navToPrev, navToServiceNotes } = useNavTo();
     const [btnLoading, setBtnLoading] = useState(false);
+
+    const clearLoadingStatus = () => {
+        dispatch({type: "SUBMIT_IN_PROGRESS", payload: false});
+        setBtnLoading(false);
+    };
 
     const submitEstimate = async () => {
         dispatch({type: "SUBMIT_IN_PROGRESS", payload: true});
         setBtnLoading(true);
         const data = {
-            serviceSource,
+            frequency,
             services,
             serviceDetails,
             serviceContact,
             serviceNotes,
+            serviceSource,
         };
-        if (services.length !== 0 && serviceContact.firstName.length !== 0 && serviceContact.firstName.length !== 0 && serviceContact.email.length !== 0 && serviceContact.phone.length === 12) {
-            const res = await fetch(`${process.env.NEXT_PUBLIC_API_DOMAIN}/servicefusion/estimates/create`, {
-                method: "POST",
-                body: JSON.stringify(data),
-            });
-            if (!res.ok) {
-                const message = await res.text();
-                dispatch({type: "SET_TAB_STATUS", payload: {id: 3, error: "true", errorMsg: message}});
-                setBtnLoading(false);
-                dispatch({type: "SUBMIT_IN_PROGRESS", payload: false});
-                return;
-            }
-            dispatch({type: "SUBMIT_IN_PROGRESS", payload: false});
-            setBtnLoading(false);
-            if (tabs[3].error) {
-                dispatch({type: "SET_TAB_STATUS", payload: {id: 3, error: "false"}});
-            }
-            dispatch({type: "SUBMIT_SERVICE"});
+        const res = await fetch(`${process.env.NEXT_PUBLIC_API_DOMAIN}/servicefusion/estimates/create`, {
+            method: "POST",
+            body: JSON.stringify(data),
+        });
+
+        if (!res.ok) {
+            const message = await res.text();
+            dispatch({type: "SET_TAB_STATUS", payload: {id: 4, error: "true", errorMsg: message}});
+            clearLoadingStatus();
+            return;
         }
+        clearLoadingStatus();
+        if (tabs[4].error) {
+            dispatch({type: "SET_TAB_STATUS", payload: {id: 4, error: "false"}});
+        }
+        dispatch({type: "SUBMISSION_SUCCESS"});
         // setTimeout(() => {
         //     dispatch({type: "SUBMIT_IN_PROGRESS", payload: false});
         //     setBtnLoading(false);
@@ -48,10 +51,10 @@ const ServiceFormButtons = () => {
             {
                 activeTab === 0 && (
                     <>
-                        <button className="bg-stone-200 text-gray-600 opacity-60 py-2 px-4 rounded  cursor-not-allowed">
+                        <button className="bg-stone-200 text-gray-600 opacity-60 py-2 px-4 rounded cursor-not-allowed">
                             Previous
                         </button>
-                        <button onClick={navToServiceDetails} className={`${services.length !== 0 ? "bg-susy hover:bg-susy text-white" : "bg-stone-200 text-gray-600 cursor-not-allowed opacity-70"} py-2 px-4 rounded`}>
+                        <button onClick={navToServices} className={`py-2 px-4 rounded ${frequency.length !== 0 ? "bg-susy hover:bg-susy text-white" : "bg-stone-200 text-gray-600 cursor-not-allowed opacity-70"}`}>
                             Next
                         </button>
                     </>
@@ -63,7 +66,7 @@ const ServiceFormButtons = () => {
                         <button className={`bg-susy hover:bg-susy text-white py-2 px-4 rounded`} onClick={navToPrev}>
                             Previous
                         </button>
-                        <button onClick={navToServiceContact} className={`bg-susy hover:bg-susy text-white py-2 px-4 rounded`}>
+                        <button onClick={navToServiceDetails} className={`bg-susy hover:bg-susy text-white py-2 px-4 rounded`}>
                             Next
                         </button>
                     </>
@@ -75,19 +78,31 @@ const ServiceFormButtons = () => {
                         <button className={`bg-susy hover:bg-susy text-white py-2 px-4 rounded`} onClick={navToPrev}>
                             Previous
                         </button>
-                        <button onClick={navToServiceNotes} className={`${serviceContact.validated ? "bg-susy hover:bg-susy text-white" : "bg-stone-200 text-gray-600 cursor-not-allowed opacity-70"} py-2 px-4 rounded`}>
+                        <button onClick={navToServiceContact} className={`bg-susy hover:bg-susy text-white py-2 px-4 rounded`}>
                             Next
                         </button>
                     </>
                 )
             }
             {
-                activeTab === 3 && !serviceSubmitted && (
+                activeTab === 3 && (
                     <>
-                        <button className={`${submitInProgress ? "bg-stone-200 text-gray-600 opacity-60 cursor-not-allowed" : "bg-susy hover:bg-susy text-white"} py-2 px-4 rounded`} onClick={navToPrev}>
+                        <button className={`bg-susy hover:bg-susy text-white py-2 px-4 rounded`} onClick={navToPrev}>
                             Previous
                         </button>
-                        <button onClick={submitEstimate} disabled={btnLoading} className={`${!btnLoading ? "hover:bg-susy" : "opacity-60 cursor-not-allowed"} bg-susy text-white button py-2 px-4 rounded`}>
+                        <button onClick={navToServiceNotes} className={`py-2 px-4 rounded ${serviceContact.validated ? "bg-susy hover:bg-susy text-white" : "bg-stone-200 text-gray-600 cursor-not-allowed opacity-70"}`}>
+                            Next
+                        </button>
+                    </>
+                )
+            }
+            {
+                activeTab === 4 && !hasSubmittedEstimateSuccessfully && (
+                    <>
+                        <button className={`${isAttemptingToSubmitEstimate ? "bg-stone-200 text-gray-600 opacity-60 cursor-not-allowed" : "bg-susy hover:bg-susy text-white"} py-2 px-4 rounded`} onClick={navToPrev}>
+                            Previous
+                        </button>
+                        <button onClick={submitEstimate} disabled={btnLoading} className={`bg-susy text-white button py-2 px-4 rounded ${!btnLoading ? "hover:bg-susy" : "opacity-60 cursor-not-allowed"}`}>
                             <div className={"flex items-center"}>
                                 <span>Finish</span>
                                 {
