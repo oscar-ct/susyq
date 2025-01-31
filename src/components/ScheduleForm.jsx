c58158207c26169f3402813198ad4fc089b95120
Oscar Castro <oscar.a.castro818@gmail.com>
2025-01-30 18:36:27 -0600

    updated state var names, tab height, tab gap

--- src/components/ScheduleForm.jsx
+++ src/components/ScheduleForm.jsx
@@ -8,14 +8,15 @@ import ScheduleFormPanelContact from "@/components/ScheduleFormPanelContact";
 import ScheduleFormPanelNotes from "@/components/ScheduleFormPanelNotes";
 import ServiceFormButtons from "@/components/ServiceFormButtons";
 import GlobalContext from "@/context/GlobalContext";
+import ScheduleFormPanelFrequency from "@/components/ScheduleFormPanelFrequency";
 
 const ScheduleForm = () => {
 
-    const { tabs, activeTab, serviceSubmitted } = useContext(GlobalContext);
+    const { tabs, activeTab, hasSubmittedEstimateSuccessfully } = useContext(GlobalContext);
 
     return (
         <div className={"min-h-[34rem] lg:pr-10"}>
-            <div className="w-full flex gap-2 md:gap-4 h-16">
+            <div className="h-full w-full flex gap-1 md:gap-4 md:h-14">
                 {
                     tabs?.map((tab, index) => {
                         return <ScheduleFormTab tabName={tab.name} key={index} id={tab.id} disabled={tab.disabled} error={tab.error}/>
@@ -25,20 +26,26 @@ const ScheduleForm = () => {
             <div className={"py-2 h-full w-full md:py-2"}>
                 <div className={"h-8 mb-2 flex justify-center items-center"}>
                     {
-                        tabs.map((tab) => {
+                        tabs?.map((tab) => {
                             if (tab.error && activeTab === tab.id) {
                                 return (
-                                    <div key={tab.id} className={"text-center leading-4 text-red-500 font-semibold"}>{tab.errorMsg}</div>
+                                    <div key={tab.id} className={"text-sm text-center leading-4 text-red-500 font-semibold md:text-base"}>{tab.errorMsg}</div>
                                 )
                             }
                         })
                     }
                 </div>
-                <div className={`${serviceSubmitted || activeTab === 0 ? "" : "border"} h-full`}>
+                <div className={`${hasSubmittedEstimateSuccessfully || activeTab === 0 || activeTab === 1 ? "" : "border"} h-full`}>
                     {
-                        activeTab === 0 ? <ScheduleFormPanelService/> : activeTab === 1 ?
-                            <ScheduleFormPanelDetails/> : activeTab === 2 ? <ScheduleFormPanelContact/> :
-                                <ScheduleFormPanelNotes/>
+                        activeTab === 0 ?
+                            <ScheduleFormPanelFrequency/>
+                                : activeTab === 1 ?
+                                    <ScheduleFormPanelService/>
+                                    : activeTab === 2 ?
+                                        <ScheduleFormPanelDetails/>
+                                        : activeTab === 3 ?
+                                        <ScheduleFormPanelContact/>
+                                            : <ScheduleFormPanelNotes/>
                     }
                 </div>
             </div>
