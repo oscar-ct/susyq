ba18cb426995c63db113e915adf1cbd09f42a88d
Oscar Castro <oscar.a.castro818@gmail.com>
2025-01-30 19:08:36 -0600

    replace star img with react icon star, minor style update

--- src/components/ScheduleGuarantees.jsx
+++ src/components/ScheduleGuarantees.jsx
@@ -1,58 +1,28 @@
 import { TiFlagOutline } from "react-icons/ti";
 import { TiLockClosed } from "react-icons/ti";
-import Image from "next/image";
-import starIcon from "@/assets/png/icons8-star-48.png";
+
+import {IoStarSharp} from "react-icons/io5";
 
 
 const ScheduleGuarantees = () => {
     return (
         <div className={"my-8 p-0 flex flex-col gap-4 sm:flex-row lg:flex-col lg:mt-0"}>
-            <div className={"p-4 bg-susy text-white w-full sm:w-4/12 lg:w-full"}>
-                <div className={"text-xl font-light"}>
+            <div className={"rounded p-4 bg-susy text-white w-full sm:w-4/12 lg:w-full"}>
+                <div className={"text-center text-xl font-light"}>
                     100% Satisfaction Guarantee
                 </div>
-                <p className={"pt-4 text-xs leading-6"}>
+                <p className={"text-center pt-4 text-xs leading-6"}>
                     If you are not completely satisfied with your cleaning, we will come back and re-clean for free.
                 </p>
                 <div className={"pt-4 flex gap-2 justify-center"}>
-                    <Image
-                        priority
-                        width={30}
-                        height={30}
-                        src={starIcon}
-                        alt={"star"}
-                    />
-                    <Image
-                        priority
-                        width={30}
-                        height={30}
-                        src={starIcon}
-                        alt={"star"}
-                    />
-                    <Image
-                        priority
-                        width={30}
-                        height={30}
-                        src={starIcon}
-                        alt={"star"}
-                    />
-                    <Image
-                        priority
-                        width={30}
-                        height={30}
-                        src={starIcon}
-                        alt={"star"}
-                    />
-                    <Image
-                        priority
-                        width={30}
-                        height={30}
-                        src={starIcon}
-                        alt={"star"}
-                    />
+                    <IoStarSharp size={30} fill={"gold"}/>
+                    <IoStarSharp size={37} fill={"gold"}/>
+                    <IoStarSharp size={45} fill={"gold"}/>
+                    <IoStarSharp size={37} fill={"gold"}/>
+                    <IoStarSharp size={30} fill={"gold"}/>
                 </div>
             </div>
-            <div className={"p-4 bg-susy text-white w-full sm:w-4/12 lg:w-full"}>
+            <div className={"text-center rounded p-4 bg-susy text-white w-full sm:w-4/12 lg:w-full"}>
                 <div className={"text-xl font-light"}>
                     We are committed to your safety
                 </div>
@@ -63,7 +33,7 @@ const ScheduleGuarantees = () => {
                     <TiFlagOutline size={80}/>
                 </div>
             </div>
-            <div className={"p-4 bg-zinc-100 border w-full sm:w-4/12 lg:w-full"}>
+            <div className={"text-center rounded p-4 bg-stone-100 border w-full sm:w-4/12 lg:w-full"}>
                 <div className={"text-xl text-gray-600 font-light"}>
                     Your privacy is important to us. We never sell or share your info!
                 </div>
