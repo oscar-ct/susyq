4a1e20e5791d5f7fdb85ec83fa1d567a488aa27f
Oscar Castro <oscar.a.castro818@gmail.com>
2025-01-30 19:07:23 -0600

    add nav-to-services function to click event action function, create new tabs for mobile, update var names

--- src/components/ScheduleFormTab.jsx
+++ src/components/ScheduleFormTab.jsx
@@ -2,37 +2,53 @@ import {useContext} from "react";
 import GlobalContext from "@/context/GlobalContext";
 import useNavToServiceDetails from "@/hooks/useNavTo";
 
-const ScheduleFormTab = ({ disabled, tabName, id, error }) => {
+const ScheduleFormTab = ({disabled, tabName, id, error}) => {
 
-    const { activeTab, services, dispatch, serviceSubmitted, submitInProgress } = useContext(GlobalContext);
-    const { navToServiceDetails, navToServiceContact, navToServiceNotes } = useNavToServiceDetails();
+    const {
+        activeTab,
+        dispatch,
+        hasSubmittedEstimateSuccessfully,
+        isAttemptingToSubmitEstimate
+    } = useContext(GlobalContext);
+    const {navToServices, navToServiceDetails, navToServiceContact, navToServiceNotes} = useNavToServiceDetails();
 
-    const onClick = () => {
-        if (id === 0 && !serviceSubmitted && !submitInProgress) {
+    const handleNavToAction = () => {
+        if (id === 0 && !hasSubmittedEstimateSuccessfully && !isAttemptingToSubmitEstimate) {
             dispatch({
                 type: "SET_ACTIVE_TAB",
                 payload: id
             });
         }
         if (id === 1) {
-            navToServiceDetails();
+            navToServices();
             return;
         }
         if (id === 2) {
-            navToServiceContact();
+            navToServiceDetails();
             return;
         }
         if (id === 3) {
+            navToServiceContact();
+            return;
+        }
+        if (id === 4) {
             navToServiceNotes();
         }
     };
 
     return (
-        <div className={"h-full w-4/12"} onClick={onClick}>
-            <div className={`${disabled ? "bg-stone-200 text-gray-600 cursor-not-allowed" : "bg-susy text-white cursor-pointer"} ${activeTab === id && !disabled ? "opacity-100" : "opacity-60"} ${error ? "!bg-red-500" : ""} ${services.length === 0 && id !== 0 ? "cursor-not-allowed" : ""} ${serviceSubmitted || submitInProgress ? "!cursor-not-allowed" : ""} h-full rounded flex items-center justify-center sm:justify-start`}>
-                <span className={"px-2 lg:px-4 text-center lg:text-start"}>{tabName}</span>
+        <>
+            {/*Desktop Tabs*/}
+            <div onClick={handleNavToAction} className={`gap-2 h-full flex flex-col items-center justify-center md:hidden ${activeTab === id ? "w-1/4" : "w-1/5"} ${disabled ? "cursor-not-allowed" : "cursor-pointer"} ${activeTab === id && !disabled ? "opacity-100" : "opacity-60"} ${hasSubmittedEstimateSuccessfully || isAttemptingToSubmitEstimate ? "!cursor-not-allowed" : ""}`}>
+                <span className={`${activeTab === id ? "text-base md:text-xl" : "text-xs md:text-base"}`}>{tabName}</span>
+                <div className={`h-1 w-full rounded-full ${disabled ? "bg-stone-300" : "bg-susy"} ${error ? "!bg-red-500" : ""}`}/>
             </div>
-        </div>
+            {/*Mobile Tabs*/}
+            <div onClick={handleNavToAction} className={`hidden w-1/5 h-full rounded md:flex items-center justify-center ${disabled ? "bg-stone-200 text-gray-600 cursor-not-allowed" : "bg-susy text-white cursor-pointer"} ${activeTab === id && !disabled ? "opacity-100" : "opacity-60"} ${error ? "!bg-red-500" : ""}  ${hasSubmittedEstimateSuccessfully || isAttemptingToSubmitEstimate ? "!cursor-not-allowed" : ""}`}>
+                <span className={"text-xs sm:text-base"}>{tabName}</span>
+            </div>
+
+        </>
     );
 };
 
