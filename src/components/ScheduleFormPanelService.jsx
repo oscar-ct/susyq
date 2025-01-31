1ddcf6b54ac80cc09f0f5c2832258944e2ecb53b
Oscar Castro <oscar.a.castro818@gmail.com>
2025-01-30 19:00:32 -0600

    create new array list for services based on frequency, remove click action, add loop to render card component based on frequency

--- src/components/ScheduleFormPanelService.jsx
+++ src/components/ScheduleFormPanelService.jsx
@@ -1,82 +1,41 @@
-import ServiceIcon from "@/components/ServiceIcon";
 import {useContext} from "react";
 import GlobalContext from "@/context/GlobalContext";
+import ScheduleFormPanelServiceCard from "@/components/ScheduleFormPanelServiceCard";
 
 const ScheduleFormPanelService = () => {
 
-    const serviceList = [
+    const oneTimeServiceList = [
         ["TiLeaf", "Deep Cleaning", " Thorough cleaning of your house. Deep cleaning ensures that your house is spotless and stays that way."],
-        ["TiHomeOutline", "Weekly, Bi-Weekly, Monthly", "Our cleaners are fully trained and experienced, they are fast and efficient in what they do to provide residential house cleaning."],
         ["TiBriefcase", "Commercial Cleaning", "Susy Q understands the importance of your company’s image and your office’s appearance."],
-        ["TiArrowMinimiseOutline", "Move In Cleaning", "We understand the stress of moving your home lets us take care of the cleaning!."],
-        ["TiArrowMaximiseOutline", "Move Out Cleaning", "Don’t want to clean up or be charged a fortune after moving out, our team works hard and we put every effort to meet your expectations."],
-        ["TiShoppingBag", "Organization", "Organization is key to keeping a tidy home, let us help."],
+        ["TiArrowMinimiseOutline", "Move In Cleaning", "We understand the stress of moving, let us handle the cleaning!"],
+        ["TiArrowMaximiseOutline", "Move Out Cleaning", "Avoid the hassle and expense of post-move cleanup, our dedicated team works tirelessly to meet your expectations."],
     ];
 
-    const { services, serviceDetails, dispatch } = useContext(GlobalContext);
+    const recurringServiceList = [
+        ["TiHomeOutline", "Residential Cleaning", "Our cleaners are fully trained and experienced, delivering fast and efficient residential house cleaning."],
+        ["TiBriefcase", "Commercial Cleaning", "Susy Q understands the importance of your company’s image and your office’s appearance."],
+    ];
 
-    const onClick = (title) => {
-        let globalContextServices;
-        if (!services.includes(title)) {
-            globalContextServices = [
-                ...services,
-                title,
-            ];
-            dispatch({
-                type: "SET_SERVICES",
-                payload: globalContextServices
-            });
-            if (title === "Weekly, Bi-Weekly, Monthly") {
-                dispatch({
-                    type: "SET_SERVICE_DETAILS",
-                    payload: {...serviceDetails, frequency: "Bi-Weekly"}
-                });
-            }
-            return;
-        }
-        globalContextServices = services.filter((string) => {
-            return string !== title;
-        });
-        dispatch({
-            type: "SET_SERVICES",
-            payload: globalContextServices
-        });
-        if (title === "Weekly, Bi-Weekly, Monthly") {
-            dispatch({
-                type: "SET_SERVICE_DETAILS",
-                payload: {...serviceDetails, frequency: ""}
-            });
-        }
-    };
+
+    const { frequency } = useContext(GlobalContext);
 
     return (
-        <>
-            <div className={""}>
-                <div className={"w-full flex flex-col md:flex-row flex-wrap"}>
-                {
-                    serviceList.map(([iconName, title, content], index) => {
-                        return (
-                            <div key={index} className={"w-full md:w-6/12"}>
-                                <div className={`px-2 pb-4 h-full`}>
-                                    <div onClick={() => onClick(title)} className={`cursor-pointer bg-stone-100 border border-stone-200 rounded p-5 w-full h-full flex ${services.includes(title) ? "!bg-susy" : "hover:bg-stone-200"}`}>
-                                        <div className={`flex items-center justify-center ${services.includes(title) ? "text-white" : "text-susy"}`}>
-                                            <ServiceIcon iconName={iconName} iconSize={60}/>
-                                        </div>
-                                        <div className={`min-h-[120px] px-2 flex flex-col font-light gap-3 justify-center ${services.includes(title) ? "text-white" : "text-gray-700"}`}>
-                                            <span className={"text-xl"}>{title}</span>
-                                            <p className={"leading-tight"}>
-                                                {content}
-                                            </p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        );
-                    })
-                }
-                </div>
-            </div>
-        </>
+        <div className={"w-full flex flex-col md:flex-row flex-wrap"}>
+        {
+            frequency === "one-time" && oneTimeServiceList.map(([iconName, title, content], index) => {
+                return (
+                   <ScheduleFormPanelServiceCard key={index} iconName={iconName} title={title} content={content} />
+                );
+            })
+        }
+        {
+            frequency === "recurring" && recurringServiceList.map(([iconName, title, content], index) => {
+                return (
+                    <ScheduleFormPanelServiceCard key={index} iconName={iconName} title={title} content={content} />
+                );
+            })
+        }
+        </div>
     );
 };
 
