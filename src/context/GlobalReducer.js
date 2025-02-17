73bb37922b7f5d96db9e89d5f28f095475c6c394
Oscar Castro <oscar.a.castro818@gmail.com>
2025-02-17 04:30:47 -0600

    create new reducer actions to save state to local storage and reset state to initial state

--- src/context/GlobalReducer.js
+++ src/context/GlobalReducer.js
@@ -1,5 +1,81 @@
+import {initialStateObj} from "@/utils/initialState";
+
 const globalReducer = (state, action) => {
     switch(action.type) {
+        case "SET_LS":
+            localStorage.setItem("susy", JSON.stringify(state));
+            return state;
+        case "RESET_STATE":
+            return {
+                isAttemptingToSubmitEstimate: false,
+                hasSubmittedEstimateSuccessfully: false,
+                frequency: "",
+                services : [],
+                serviceDetails: {
+                    rooms: {
+                        bedroom: "3",
+                        bathroom: "2"
+                    },
+                    size: "",
+                    extras: [],
+                    frequency: "",
+                },
+                serviceContact: {
+                    validated: false,
+                    validating: false,
+                    cleaningAddress: {
+                        address1: "",
+                        address2: "",
+                        city: "",
+                        state: "TX",
+                        zipCode: ""
+                    },
+                    firstName: "",
+                    lastName: "",
+                    email: "",
+                    phone: "",
+                },
+                serviceNotes: "",
+                activeTab: 0,
+                tabs: [
+                    {
+                        id: 0,
+                        name: "Frequency",
+                        disabled: false,
+                        error: false,
+                        errorMsg: "",
+                    },
+                    {
+                        id: 1,
+                        name: "Services",
+                        disabled: true,
+                        error: false,
+                        errorMsg: "",
+                    },
+                    {
+                        id: 2,
+                        name: "Details",
+                        disabled: true,
+                        error: false,
+                        errorMsg: "",
+                    },
+                    {
+                        id: 3,
+                        name: "Contact",
+                        disabled: true,
+                        error: false,
+                        errorMsg: "",
+                    },
+                    {
+                        id: 4,
+                        name: "Notes",
+                        disabled: true,
+                        error: false,
+                        errorMsg: "",
+                    },
+                ],
+                serviceSource: "",
+            };
         case "SET_SERVICE_SOURCE":
             return {
                 ...state,
