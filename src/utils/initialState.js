220285f9a942c522cd10b9befe95d013562f1a65
Oscar Castro <oscar.a.castro818@gmail.com>
2025-02-17 04:57:25 -0600

    create initial state const export for reusability

---
+++ src/utils/initialState.js
@@ -0,0 +1,70 @@
+export const initialStateObj = {
+    isAttemptingToSubmitEstimate: false,
+    hasSubmittedEstimateSuccessfully: false,
+    frequency: "",
+    services : [],
+    serviceDetails: {
+        rooms: {
+            bedroom: "3",
+            bathroom: "2"
+        },
+        size: "",
+        extras: [],
+        frequency: "",
+    },
+    serviceContact: {
+        validated: false,
+        validating: false,
+        cleaningAddress: {
+            address1: "",
+            address2: "",
+            city: "",
+            state: "TX",
+            zipCode: ""
+        },
+        firstName: "",
+        lastName: "",
+        email: "",
+        phone: "",
+    },
+    serviceNotes: "",
+    activeTab: 0,
+    tabs: [
+        {
+            id: 0,
+            name: "Frequency",
+            disabled: false,
+            error: false,
+            errorMsg: "",
+        },
+        {
+            id: 1,
+            name: "Services",
+            disabled: true,
+            error: false,
+            errorMsg: "",
+        },
+        {
+            id: 2,
+            name: "Details",
+            disabled: true,
+            error: false,
+            errorMsg: "",
+        },
+        {
+            id: 3,
+            name: "Contact",
+            disabled: true,
+            error: false,
+            errorMsg: "",
+        },
+        {
+            id: 4,
+            name: "Notes",
+            disabled: true,
+            error: false,
+            errorMsg: "",
+        },
+    ],
+    serviceSource: "",
+};
