5c3a0b94ebd9a607a775273f2cd69fb104619878
Oscar Castro <oscar.a.castro818@gmail.com>
2025-02-17 05:03:04 -0600

    create component mount state to render jsx based on global state, setup loader when unmounted, add use effect to scroll to top on page load

--- src/components/ScheduleForm.jsx
+++ src/components/ScheduleForm.jsx
@@ -1,7 +1,7 @@
 "use client"
 
 import ScheduleFormTab from "@/components/ScheduleFormTab";
-import {useContext} from "react";
+import {useCallback, useContext, useEffect, useState} from "react";
 import ScheduleFormPanelService from "@/components/ScheduleFormPanelService";
 import ScheduleFormPanelDetails from "@/components/ScheduleFormPanelDetails";
 import ScheduleFormPanelContact from "@/components/ScheduleFormPanelContact";
@@ -10,48 +10,62 @@ import ServiceFormButtons from "@/components/ServiceFormButtons";
 import GlobalContext from "@/context/GlobalContext";
 import ScheduleFormPanelFrequency from "@/components/ScheduleFormPanelFrequency";
 
+
 const ScheduleForm = () => {
+    // on window reload, scroll to top
+    const resetWindowScrollPosition = useCallback(() => window.scrollTo(0, 0), [])
+    useEffect(() => {
+        window.onbeforeunload = function () {
+            resetWindowScrollPosition();
+        }
+    }, [resetWindowScrollPosition]);
 
     const { tabs, activeTab, hasSubmittedEstimateSuccessfully } = useContext(GlobalContext);
+    const [mounted, setMounted] = useState(false);
+    useEffect(() => {setMounted(true);}, []);
 
     return (
         <div className={"min-h-[34rem] lg:pr-10"}>
-            <div className="h-full w-full flex gap-1 md:gap-4 md:h-14">
-                {
-                    tabs?.map((tab, index) => {
-                        return <ScheduleFormTab tabName={tab.name} key={index} id={tab.id} disabled={tab.disabled} error={tab.error}/>
-                    })
-                }
-            </div>
+            <ScheduleFormTab/>
             <div className={"py-2 h-full w-full md:py-2"}>
                 <div className={"h-8 mb-2 flex justify-center items-center"}>
                     {
-                        tabs?.map((tab) => {
+                        mounted && tabs?.map((tab) => {
                             if (tab.error && activeTab === tab.id) {
                                 return (
-                                    <div key={tab.id} className={"text-sm text-center leading-4 text-red-500 font-semibold md:text-base"}>{tab.errorMsg}</div>
-                                )
+                                    <div key={tab.id} className={"text-sm text-center leading-4 text-red-500 font-semibold md:text-base"}>
+                                        {tab.errorMsg}
+                                    </div>
+                                );
                             }
                         })
                     }
                 </div>
-                <div className={`${hasSubmittedEstimateSuccessfully || activeTab === 0 || activeTab === 1 ? "" : "border"} h-full`}>
-                    {
-                        activeTab === 0 ?
-                            <ScheduleFormPanelFrequency/>
-                                : activeTab === 1 ?
-                                    <ScheduleFormPanelService/>
-                                    : activeTab === 2 ?
-                                        <ScheduleFormPanelDetails/>
-                                        : activeTab === 3 ?
-                                        <ScheduleFormPanelContact/>
-                                            : <ScheduleFormPanelNotes/>
-                    }
-                </div>
+                {
+                    !mounted ? (
+                        <div className={"h-52 flex items-center justify-center"}>
+                            <span className="loading-lg"/>
+                        </div>
+                    ) : (
+                        <div className={`${hasSubmittedEstimateSuccessfully || activeTab === 0 || activeTab === 1 ? "" : "border"} h-full`}>
+                            {
+                                activeTab === 0 ?
+                                    <ScheduleFormPanelFrequency/>
+                                    : activeTab === 1 ?
+                                        <ScheduleFormPanelService/>
+                                        : activeTab === 2 ?
+                                            <ScheduleFormPanelDetails/>
+                                            : activeTab === 3 ?
+                                                <ScheduleFormPanelContact/>
+                                                : <ScheduleFormPanelNotes/>
+                            }
+                        </div>
+                    )
+                }
             </div>
             <div className={"w-full flex items-end justify-center pt-4 gap-4 md:justify-end"}>
                 {/*{*/}
-                {/*    tabs.map((tab) => {*/}
+                {/*    mounted && tabs?.map((tab) => {*/}
                 {/*        if (tab.error && activeTab === tab.id) {*/}
                 {/*            return (*/}
                 {/*                <span key={tab.id} className={"flex-grow text-center leading-4 md:text-xl text-red-500 font-semibold"}>{tab.errorMsg}</span>*/}
@@ -59,7 +73,11 @@ const ScheduleForm = () => {
                 {/*        }*/}
                 {/*    })*/}
                 {/*}*/}
-                <ServiceFormButtons/>
+                {
+                    mounted && (
+                        <ServiceFormButtons/>
+                    )
+                }
             </div>
         </div>
     );
