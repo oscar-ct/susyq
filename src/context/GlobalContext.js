c53fbbe3637cba4c43a0ff4e0f04f3c45cbbbe2f
Oscar Castro <oscar.a.castro818@gmail.com>
2025-02-17 04:29:16 -0600

    setup initial global state to use local storage, import state obj

--- src/context/GlobalContext.js
+++ src/context/GlobalContext.js
@@ -2,81 +2,13 @@
 
 import {createContext, useReducer} from "react";
 import globalReducer from "@/context/GlobalReducer";
+import {initialStateObj} from "@/utils/initialState";
 
 
 const GlobalContext = createContext();
 
 export function GlobalProvider({ children }) {
-    const initialState = {
-        isAttemptingToSubmitEstimate: false,
-        hasSubmittedEstimateSuccessfully: false,
-        frequency: "",
-        services : [],
-        serviceDetails: {
-            rooms: {
-                bedroom: "3",
-                bathroom: "2"
-            },
-            size: "",
-            extras: [],
-            frequency: "",
-        },
-        serviceContact: {
-            validated: false,
-            validating: false,
-            cleaningAddress: {
-                address1: "",
-                address2: "",
-                city: "",
-                state: "TX",
-                zipCode: ""
-            },
-            firstName: "",
-            lastName: "",
-            email: "",
-            phone: "",
-        },
-        serviceNotes: "",
-        activeTab: 0,
-        tabs: [
-            {
-                id: 0,
-                name: "Frequency",
-                disabled: false,
-                error: false,
-                errorMsg: "",
-            },
-            {
-                id: 1,
-                name: "Services",
-                disabled: true,
-                error: false,
-                errorMsg: "",
-            },
-            {
-                id: 2,
-                name: "Details",
-                disabled: true,
-                error: false,
-                errorMsg: "",
-            },
-            {
-                id: 3,
-                name: "Contact",
-                disabled: true,
-                error: false,
-                errorMsg: "",
-            },
-            {
-                id: 4,
-                name: "Notes",
-                disabled: true,
-                error: false,
-                errorMsg: "",
-            },
-        ],
-        serviceSource: "",
-    };
+    const initialState = typeof window !== "undefined" && localStorage.getItem("susy") ? JSON.parse(localStorage.getItem("susy")) : initialStateObj;
     const [state, dispatch] = useReducer(globalReducer, initialState);
     return (
         <GlobalContext.Provider value={{
