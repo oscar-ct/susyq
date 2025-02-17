363cd0b001dacb12c84905a95533fe7acce5906f
Oscar Castro <oscar.a.castro818@gmail.com>
2025-02-17 05:10:32 -0600

    create component mount state to render jsx based on global state, setup dummy tabs when unmounted,

--- src/components/ScheduleFormTab.jsx
+++ src/components/ScheduleFormTab.jsx
@@ -1,23 +1,27 @@
-import {useContext} from "react";
+import {useContext, useEffect, useState} from "react";
 import GlobalContext from "@/context/GlobalContext";
 import useNavToServiceDetails from "@/hooks/useNavTo";
+import {initialStateObj} from "@/utils/initialState";
 
-const ScheduleFormTab = ({disabled, tabName, id, error}) => {
+const ScheduleFormTab = () => {
 
     const {
         activeTab,
+        tabs,
         dispatch,
         hasSubmittedEstimateSuccessfully,
         isAttemptingToSubmitEstimate
     } = useContext(GlobalContext);
+
     const {navToServices, navToServiceDetails, navToServiceContact, navToServiceNotes} = useNavToServiceDetails();
 
-    const handleNavToAction = () => {
+    const handleNavToAction = (id) => {
         if (id === 0 && !hasSubmittedEstimateSuccessfully && !isAttemptingToSubmitEstimate) {
             dispatch({
                 type: "SET_ACTIVE_TAB",
                 payload: id
             });
+            dispatch({ type: "SET_LS" });
         }
         if (id === 1) {
             navToServices();
@@ -36,19 +40,56 @@ const ScheduleFormTab = ({disabled, tabName, id, error}) => {
         }
     };
 
-    return (
-        <>
-            {/*Desktop Tabs*/}
-            <div onClick={handleNavToAction} className={`gap-2 h-full flex flex-col items-center justify-center md:hidden ${activeTab === id ? "w-1/4" : "w-1/5"} ${disabled ? "cursor-not-allowed" : "cursor-pointer"} ${activeTab === id && !disabled ? "opacity-100" : "opacity-60"} ${hasSubmittedEstimateSuccessfully || isAttemptingToSubmitEstimate ? "!cursor-not-allowed" : ""}`}>
-                <span className={`${activeTab === id ? "text-base md:text-xl" : "text-xs md:text-base"}`}>{tabName}</span>
-                <div className={`h-1 w-full rounded-full ${disabled ? "bg-stone-300" : "bg-susy"} ${error ? "!bg-red-500" : ""}`}/>
-            </div>
-            {/*Mobile Tabs*/}
-            <div onClick={handleNavToAction} className={`hidden w-1/5 h-full rounded md:flex items-center justify-center ${disabled ? "bg-stone-200 text-gray-600 cursor-not-allowed" : "bg-susy text-white cursor-pointer"} ${activeTab === id && !disabled ? "opacity-100" : "opacity-60"} ${error ? "!bg-red-500" : ""}  ${hasSubmittedEstimateSuccessfully || isAttemptingToSubmitEstimate ? "!cursor-not-allowed" : ""}`}>
-                <span className={"text-xs sm:text-base"}>{tabName}</span>
-            </div>
+    const [mounted, setMounted] = useState(false);
+    useEffect(() => {setMounted(true);}, []);
 
-        </>
+    return (
+        <div className="h-full w-full flex gap-1 md:gap-4 md:h-14">
+            {
+                !mounted ? (
+                    <>
+                        {
+                            initialStateObj.tabs.map((tab, index) => (
+                                <div key={index} className={"w-1/5"}>
+                                    {/*Mobile Tabs*/}
+                                    <div key={index} className={"gap-2 w-full h-full flex flex-col items-center justify-center md:hidden cursor-not-allowed opacity-40"}>
+                                        <span className={"text-xs md:text-base"}>{tab.name}</span>
+                                        <div className={"h-1 w-full rounded-full bg-stone-300"}/>
+                                    </div>
+
+                                    {/*Desktop Tabs*/}
+                                    <div key={tab.name} className={"hidden w-full h-full rounded md:flex items-center justify-center bg-stone-200 text-gray-600 cursor-not-allowed opacity-40"}>
+                                        <span className={"text-xs sm:text-base"}>{tab.name}</span>
+                                    </div>
+                                </div>
+                            ))
+                        }
+                    </>
+                ) : (
+                    <>
+                        {
+                            tabs.map((tab, index) => (
+                                <div key={index} className={` ${activeTab === tab.id ? "w-1/4" : "w-1/5"} md:w-1/5`}>
+                                    {/*Mobile Tabs*/}
+                                    <div key={index} onClick={() => handleNavToAction(tab.id)}
+                                         className={`gap-2 h-min flex flex-col items-center justify-center md:hidden ${tab.disabled ? "cursor-not-allowed" : "cursor-pointer"} ${activeTab === tab.id && !tab.disabled ? "opacity-100" : "opacity-60"} ${hasSubmittedEstimateSuccessfully || isAttemptingToSubmitEstimate ? "!cursor-not-allowed" : ""}`}
+                                    >
+                                        <span className={`${activeTab === tab.id ? "text-base md:text-xl" : "text-xs md:text-base"}`}>{tab.name}</span>
+                                        <div className={`h-1 w-full rounded-full ${tab.disabled ? "bg-stone-300" : "bg-susy"} ${tab.error ? "!bg-red-500" : ""}`}/>
+                                    </div>
+                                    {/*Desktop Tabs*/}
+                                    <div onClick={() => handleNavToAction(tab.id)}
+                                         className={`hidden w-full h-full rounded md:flex items-center justify-center ${tab.disabled ? "bg-stone-200 text-gray-600 cursor-not-allowed" : "bg-susy text-white cursor-pointer"} ${activeTab === tab.id && !tab.disabled ? "opacity-100" : "opacity-60"} ${tab.error ? "!bg-red-500" : ""}  ${hasSubmittedEstimateSuccessfully || isAttemptingToSubmitEstimate ? "!cursor-not-allowed" : ""}`}
+                                    >
+                                        <span className={"text-xs sm:text-base"}>{tab.name}</span>
+                                    </div>
+                                </div>
+                            ))
+                        }
+                    </>
+                )
+            }
+        </div>
     );
 };
 
