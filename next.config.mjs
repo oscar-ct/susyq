1a03c266505e8d685d0a78f7efa25e4d8839266c
Oscar Castro <oscar.a.castro818@gmail.com>
2025-04-07 17:26:58 -0500

    feat: add permanent wildcard redirect

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async redirects() {
+        return [
+            {
+                source: '/cleaning_services_austin/:slug',
+                destination: '/',
+                permanent: true,
+            },
+        ]
+    },
+};
 
 export default nextConfig;
