9d34eb3b015b5a11f53a6e11660b6166a2897783
Oscar Castro <oscar.a.castro818@gmail.com>
2025-04-10 21:56:57 -0500

    feat: remove unused values from Mongoose Schema

--- src/models/credentials.js
+++ src/models/credentials.js
@@ -2,8 +2,6 @@ import mongoose from "mongoose";
 
 const Credentials = mongoose.Schema(
     {
-        goHighLevelAuthToken: String,
-        goHighLevelRefreshToken: String,
         serviceFusionAuthToken: String,
         serviceFusionRefreshToken: String,
         serviceFusionJWT: String,
