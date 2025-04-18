289580594bfa49792d6b9849ca3c946ccadd4547
Oscar Castro <oscar.a.castro818@gmail.com>
2025-04-18 00:09:03 -0500

    feat: clean up controller, moved api calls to utils, moved api error handling to api functions, added error catching for unhandled exceptions

--- src/app/api/servicefusion/customers/create/route.js
+++ src/app/api/servicefusion/customers/create/route.js
@@ -1,114 +1,52 @@
 import connectDB from "@/lib/connectDB";
 import Credentials from "@/models/credentials";
-
-const fetchServiceFusionAccessToken = async (refreshToken) => {
-    const res = await fetch("https://api.servicefusion.com/oauth/access_token", {
-        method: "POST",
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            "grant_type": "refresh_token",
-            "refresh_token": refreshToken
-        }),
-    });
-    if (!res.ok) {
-        return null;
-    }
-    return await res.json()
-};
-
-const fetchServiceFusionCheckIfCustomerExists = async (email, token) => {
-    const res = await fetch(`https://api.servicefusion.com/v1/customers?per-page=1&access_token=${token}&filters[email]=${email}`, {
-        method: "GET",
-        headers: {
-            'Accept': 'application/json'
-        },
-    });
-    if (!res.ok) {
-        return null;
-    }
-    return await res.json()
-};
-
-const fetchServiceFusionCreateNewCustomer = async (customer, token) => {
-    const res = await fetch('https://api.servicefusion.com/v1/customers', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${token}`,
-            'Accept': 'application/json'
-        },
-        body: JSON.stringify(customer),
-    });
-    if (!res.ok && res.status === 500) {
-        return 500;
-    }
-    if (!res.ok) {
-        // const msg = await res.json()
-        // console.log(msg)
-        return null;
-    }
-    return await res.json()
-};
-
+import {
+    fetchServiceFusionAccessToken,
+    fetchServiceFusionCheckIfCustomerExists,
+    fetchServiceFusionCreateNewCustomer
+} from "@/utils/serviceFusionApi";
 
 export async function POST(req) {
     try {
         await connectDB();
-    } catch {
-        return new Response("Something went wrong connecting to db", {status: 404});
-    }
-    const credentials = await Credentials.findById("66dad17f465d12d0ab01513d");
-    const updatedServiceFusionAccessTokens = await fetchServiceFusionAccessToken(credentials.serviceFusionRefreshToken);
-    if (!updatedServiceFusionAccessTokens) {
-        return new Response("Something went wrong (/access_token)", {status: 404});
-    } else {
-        const { access_token, refresh_token } = updatedServiceFusionAccessTokens;
-        credentials.serviceFusionRefreshToken = refresh_token;
-        credentials.serviceFusionAccessToken = access_token;
-        await credentials.save();
-        const { name, lastName, phone, email } = await req.json();
-        const existingCustomer = await fetchServiceFusionCheckIfCustomerExists(email, access_token);
-        if (!existingCustomer) {
-            return new Response("Something went wrong (/customer/get)", {status: 404});
-        } else {
-            if (existingCustomer.items.length !== 0) {
-              return Response.json({
-                  contactCreated: false,
-                  message: `${name} ${lastName} was found as an existing contact in Service Fusion.`
-              });
-            } else {
-                // const firstName = name.split(' ').slice(0, -1).join(' ');
-                // const lastName = name.split(' ').slice(-1).join(' ');
-                let newCustomerBody = {
-                    customer_name: `${name} ${lastName}`,
-                    contacts: [{
-                        fname: name,
-                        lname: lastName,
-                        phones: [{
-                            phone: phone,
-                            type: "Mobile"
-                        }],
-                        emails: [{
-                            email: email,
-                            class: "Personal"
-                        }],
-                        is_primary: "true"
-                    }],
-                    referral_source: "susyqonline",
-                    is_taxable: true,
-                };
-                const newCustomer = await fetchServiceFusionCreateNewCustomer(newCustomerBody, access_token);
-                if (!newCustomer) {
-                    return new Response("Something went wrong (/customer/post)", {status: 404});
-                } else {
-                    return Response.json({
-                        contactCreated: true,
-                        message: `A new contact for ${name} ${lastName} has been created in Service Fusion.`
-                    });
-                }
-            }
+        const serviceFusionCredentials = await Credentials.findById("66dad17f465d12d0ab01513d");
+        const tokenResponse = await fetchServiceFusionAccessToken(serviceFusionCredentials.serviceFusionRefreshToken);
+        if (!tokenResponse.success) {
+            return new Response(tokenResponse.message, {status: 505});
         }
+        const { access_token, refresh_token } = tokenResponse.data;
+        serviceFusionCredentials.serviceFusionRefreshToken = refresh_token;
+        serviceFusionCredentials.serviceFusionAccessToken = access_token;
+        await serviceFusionCredentials.save();
+        const {firstName, lastName, phone, email} = await req.json();
+        const checkResponse = await fetchServiceFusionCheckIfCustomerExists(email, access_token);
+        if (!checkResponse.success) {
+            return new Response(checkResponse.message, {status: 500});
+        }
+        if (checkResponse.message !== "Customer not found") {
+            return new Response(checkResponse.message);
+        }
+        const newCustomerPayload = {
+            customer_name: `${firstName} ${lastName}`,
+            contacts: [
+                {
+                    fname: firstName,
+                    lname: lastName,
+                    phones: [{phone: phone, type: "Mobile"}],
+                    emails: [{email: email, class: "Personal"}],
+                    is_primary: "true"
+                }
+            ],
+            referral_source: "susyqonline",
+            is_taxable: true,
+        };
+        const createResponse = await fetchServiceFusionCreateNewCustomer(newCustomerPayload, access_token);
+        if (!createResponse.success) {
+            return new Response(createResponse.message, {status: 500});
+        }
+        return new Response(createResponse.message);
+    } catch (error) {
+        console.error("API Error:", error.message);
+        return new Response(`Error: ${error.message}`, {status: 500});
     }
 }
